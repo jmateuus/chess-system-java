ccebe5954a8745a6ff9df9f607fe67ea2ca09548
jmmelo2004 <jmmelo2004@gmail.com>
2023-05-23 18:16:04 -0300

    ChessException and chessPosition

---
+++ src/chess/ChessExcption.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessExcption extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExcption(String msg) {
+		super(msg);
+	}
+	
+}
